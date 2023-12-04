1b8d3e7bf12b49112d971b89d7f528beaa1a257e
FinnCallies <finn.callies@gmail.com>
2023-12-04 14:32:04 +0100

    init

---
+++ Day1/day1easy.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int line2num(char* line) {
+    char str[2] = { 0 };
+    int num = 0;
+    int length = strlen(line);
+    /*
+        int length = ((int)(strchr(line, '\n') - line)) - 1;
+    
+        custom strlen function
+        does not work for last line, because of the missing \n
+        the string probably ends with \0 though, but not tested 
+    */ 
+    
+    for (int i = 0; i < length; i++) {
+        if (line[i] >= '0' && line[i] <= '9') {
+            str[0] = line[i];
+            printf("[%d] : \"%c\", ", i, line[i]);
+            i = length;
+        }
+    }
+    
+    for (int i = length - 1; i > -1; i--) {
+        if (line[i] >= '0' && line[i] <= '9') {
+            str[1] = line[i];
+            printf("[%d] : \"%c\", ", i, line[i]);
+            i = -1;
+        }
+    }
+    num = atoi(str);
+    printf("num: %d, ", num);
+
+    return num;
+}
+
+
+void main(int argc, char* argv[])
+{
+    FILE* fp;
+    char* line = NULL;
+    size_t len = 0;
+    int sum = 0;
+    int counter = 1;
+
+    fp = fopen(argv[1], "r");
+    if (fp == NULL)
+        exit(EXIT_FAILURE);
+
+
+
+    while (getline(&line, &len, fp) != -1) {
+        printf("%d: ", counter);
+        int num = line2num(line);
+        sum += num;
+        printf("sum: %d\n", sum);
+        counter++;
+    }
+
+    printf("sum: %d\n", sum);
+}
